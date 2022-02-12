9cad00b4bff43b45bd00a2bc5fecd3ece9de3a80
Akhil Kamsala <87219018+akhilsdeportfolio@users.noreply.github.com>
2022-02-12 17:23:57 +0530

    making it better

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
+const axios = require('axios');
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
+
+  app.on("issues.opened", async (context) => {    
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Thanks for opening this issue our team will get back to you shortly!"
     });
     return context.octokit.issues.createComment(issueComment);
+
+
+
+
+
+
+
+
+
+
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+
+
+
 };
+
+
